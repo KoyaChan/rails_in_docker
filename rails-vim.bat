2fafcbae6e68c5f6f5aae58bd1dbc9750de2f8ec
KoyaChan <koyto01@yahoo.co.jp>
2018-09-11 19:06:08 +0900

    use koyachan/railstutorial image

--- rails-vim.bat
+++ rails-vim.bat
@@ -4,6 +4,7 @@ cd e:\work\rails-vim
 @REM docker-compose run -e TZ=Asia/Tokyo rails
 @REM docker-compose rm
 
-@REM docker run --name RailsTutorial -it -v e:\work\scripts\RailsTutorial:/usr/src/scripts -p 3000:3000 koyachan/rails-vim
-docker container start RailsTutorial
-docker container attach RailsTutorial
+docker run --name RailsTutorial -it -v e:\work\scripts\RailsTutorial:/usr/src/scripts -p 3000:3000 koyachan/railstutorial
+docker container prune
+@REM docker container start RailsTutorial
+@REM docker container attach RailsTutorial
