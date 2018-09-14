1d20ad0f539c61d0a25b726571137f3c509b5248
KoyaChan <koyto01@yahoo.co.jp>
2018-09-14 16:38:35 +0900

    correct volume path

--- rails-vim.bat
+++ rails-vim.bat
@@ -4,7 +4,7 @@ cd e:\work\rails-vim
 @REM docker-compose run -e TZ=Asia/Tokyo rails
 @REM docker-compose rm
 
-docker run --name RailsTutorial -it -v e:\work\scripts\RailsTutorial:/usr/src/scripts -p 3000:3000 koyachan/railstutorial
+docker run --name RailsTutorial -it -v e:\work\scripts\RailsTutorial:/usr/src/scripts -v e:\work\rails-vim:/usr/src/rails-vim -p 3000:3000 koyachan/railstutorial
 docker container prune
 @REM docker container start RailsTutorial
 @REM docker container attach RailsTutorial
